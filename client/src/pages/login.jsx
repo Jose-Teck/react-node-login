3a1234f51594cd1d3011f84f8a602c012af499e1
Jose-Teck <joseteck03@gmail.com>
2026-01-08 20:23:22 -0600

    style the Ui

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -12,14 +12,30 @@ const handleLogin = async () => {
     const data = await res.json();
     console.log(data);
   };
-
+  function togglePassword(){
+    const passwordInput = document.querySelector('input[type="password"]');
+    const icon = document.querySelector('.bi-eye');
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text";
+        icon.classList.remove('bi-eye');
+        icon.classList.add('bi-eye-slash');
+    } else {
+        passwordInput.type = "password";
+        icon.classList.remove('bi-eye-slash');
+        icon.classList.add('bi-eye');
+    }
+  }
   function Login(){
     return (
-        <div>
+        <div className="login-container">
         <h2>Login</h2>
-        <input placeholder="Email" />
-        <input placeholder="Password" />
-        <button onClick={handleLogin}>Login</button>
+        <div className="input"><input placeholder="Username" required />
+        <i className="bi-person"></i>
+        </div>
+        <div className="input"><input placeholder="Password" type="password"/>
+        <i className=" bi-eye"></i></div>
+        <div className="btns"><button onClick={handleLogin}>Login</button>
+        <button>Sign Up?</button></div>
       </div>
     );
 }
