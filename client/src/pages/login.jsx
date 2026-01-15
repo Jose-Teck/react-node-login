ef74e499da647ce53fe2383ca6c0b080f7f36ea5
Jose-Teck <joseteck03@gmail.com>
2026-01-14 22:09:58 -0600

    added more style

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,4 +1,5 @@
 
+import heroBG from '../assets/hero-bg.jpg';
 const handleLogin = async () => {
     const res = await fetch("http://localhost:5000/login", {
       method: "POST",
@@ -27,16 +28,55 @@ const handleLogin = async () => {
   }
   function Login(){
     return (
-        <div className="login-container">
-        <h2>Login</h2>
-        <div className="input"><input placeholder="Username" required />
-        <i className="bi-person"></i>
+        <div className="container">
+          <div className="hero-section">
+            <img src={heroBG} alt="cacao" className="hero-bg" />
+            <div className='bg-overlay'>
+            <div className='mini-logo'>
+              <img src="#" alt="logo" className="logo-hero"/>
+            <p>Belizean Cacao</p>
+            </div>
+            <h1>The Spirit of the <br /> <i>jungle</i> in every <br /> bean.</h1>
+            <p>join our collective of chocolate artisans and eco-traveleres. Explore
+              the heritage of the Mayan Mountains
+            </p>
+            </div>
+          </div>
+          <div className="login-form">
+            <h2>Welcome Back</h2>
+            <p>SIGN IN</p>
+            <p>Register</p>
+            <form action="#">
+              <div className="input-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Enter your email" required/>
+              </div>
+              <div className="input-group">
+                <label htmlFor="password">Password</label>
+                <div className="password-wrapper">
+                  <input type="password" id="password" name="password" placeholder="Enter your password" required/>
+                  <i className="bi bi-eye" onClick={togglePassword}></i>
+                </div>
+              </div>
+              <div className="options">
+                <div className="remember-me">
+                  <input type="checkbox" id="remember-me" name="remember-me"/>
+                  <label htmlFor="remember-me">Remember Me</label>
+                </div>
+                <a href="#">Forgot Password?</a>
+              </div>
+              <button type="submit" onClick={handleLogin}>Login</button>
+              <div className="login-footer">
+                <p>OR CONTINUE WITH</p>
+                <div className="social-icons">
+                  <a href="#"><i className="bi bi-google"></i></a>
+                  <p>Google</p>
+                  
+                 </div>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="input"><input placeholder="Password" type="password"/>
-        <i className=" bi-eye"></i></div>
-        <div className="btns"><button onClick={handleLogin}>Login</button>
-        <button>Sign Up?</button></div>
-      </div>
     );
 }
 
